12778c9fd9bdffc43697c76c25b1cdfcb23d1609
nataliarosa88 <42919658+nataliarosa88@users.noreply.github.com>
2019-02-27 18:11:34 -0300

    Sem JPAUtil

--- br/com/andretecnologia/makeup/test/CustomerTest.java
+++ br/com/andretecnologia/makeup/test/CustomerTest.java
@@ -1,29 +1,20 @@
 package br.com.andretecnologia.makeup.test;
 
 import javax.persistence.EntityManager;
-
-import br.com.andretecnologia.makeup.util.JPAUtil;
-import br.com.andretecnologia.makeup.model.Customer;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;import br.com.andretecnologia.makeup.model.Customer;
 
 public class CustomerTest {
 	public static void main(String[] args) {
 		Customer c1 = new Customer();
-
 		c1.setName("André");
-
-				EntityManager em = new JPAUtil().getEntityManager();
-
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("makeup");
+		EntityManager em = entityManagerFactory.createEntityManager();
 		em.getTransaction().begin();
-
 		em.persist(c1);
-
 		em.getTransaction().commit();
-
+		Customer c = em.find(Customer.class, 1);
+		System.out.println(c.getName());
 		em.close();
-	
-		
-		
-		
-		
 	}
 }
